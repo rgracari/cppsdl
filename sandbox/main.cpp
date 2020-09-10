598f1504edab7ad3d860201743141dcbb4e8da92
Raphael Ribeiro <raphael.ribeiro.pro@gmail.com>
2020-09-10 12:24:20 +0200

    Adding flags for init context

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    cppsdl::Context context;
+    cppsdl::Context context(cppsdl::CONTEXT_FLAGS_INIT::VIDEO);
     cppsdl::Window window(
         "TEST",
         800,
--- src/context.h
+++ src/context.h
@@ -3,13 +3,26 @@
 
 namespace cppsdl
 {
+    namespace CONTEXT_FLAGS_INIT
+    {
+        constexpr uint32_t TIMER = SDL_INIT_TIMER;
+        constexpr uint32_t AUDIO = SDL_INIT_AUDIO;
+        constexpr uint32_t VIDEO = SDL_INIT_VIDEO;
+        constexpr uint32_t JOYSTICK = SDL_INIT_JOYSTICK;
+        constexpr uint32_t HAPTIC = SDL_INIT_HAPTIC;
+        constexpr uint32_t GAMECONTROLLER = SDL_INIT_GAMECONTROLLER;
+        constexpr uint32_t EVENTS = SDL_INIT_EVENTS;
+        constexpr uint32_t EVERYTHING = SDL_INIT_EVERYTHING;
+        constexpr uint32_t NOPARACHUTE = SDL_INIT_NOPARACHUTE;
+    } // namespace CONTEXT_FLAGS_INIT
+
     class Context
     {
     public:
-        Context()
+        Context(uint32_t flags = CONTEXT_FLAGS_INIT::VIDEO)
         {
             std::cout << "SDL_INIT" << std::endl;
-            SDL_Init(SDL_INIT_VIDEO);
+            SDL_Init(flags);
         }
         ~Context()
         {
